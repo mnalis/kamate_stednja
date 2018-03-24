eb0d004045aa5c613439c808766c8deeeb65971d
Matija Nalis <mnalis-git@voyager.hr>
2018-03-24 22:40:20 +0100

    raiffeisenstambena.hr, po EKS primjerima

--- banks/stambena-rba.pm
+++ banks/stambena-rba.pm
@@ -1,3 +1,6 @@
-my $html = fetch_url('https://www.raiffeisenstambena.hr/stambena-stednja');
-if ($html =~ /(.*)Primjer izra/si) { $html = $1 } else { die "$html\ndoesn't match our regexp" }
-return find_max_kamata_regex($html, '\b([1-9]?\d)[.,](\d{1,2})\s*%');
+return best_kamata_html_mpp ('https://www.raiffeisenstambena.hr/stambena-stednja',
+	'Primjer izra(.*)se temelji na pretpostavci',
+	'EKS.*?%.*?<td>\s*',
+	'\s*</td>'
+);
+
