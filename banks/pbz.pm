ccad49007273c5e1eb759b1548a0607d72667f21
Matija Nalis <mnalis-git@voyager.hr>
2018-04-19 17:02:34 +0200

    izvacena nerealna e-Perspektiv

--- banks/pbz.pm
+++ banks/pbz.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.pbz.hr/hr/gradani/stednja');
 $mech->follow_link( text_regex => qr/kamatnim stopama/i );
-return best_kamata_pdf_mpp('(.*?)Premium invest', '^', '\s*$');
+return best_kamata_pdf_mpp('(.*?)e-Perspektiva.*?maksimalni iznos(.*?)Premium invest.*?inimalni iznos(.*)', '^', '\s*$');
