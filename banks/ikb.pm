c78852907838b6ebd11ece704ca094c21f285295
Matija Nalis <mnalis-git@voyager.hr>
2018-04-19 10:16:09 +0200

    ikb nove stranice

--- banks/ikb.pm
+++ banks/ikb.pm
@@ -1,4 +1,3 @@
-my $html = fetch_url('http://www.ikb.hr/Default.aspx?art=560');
-$mech->follow_link( text_regex => qr/kamatne stope na depozite/i );
-return best_kamata_pdf_mpp('POLOGE U KUNAMA(.*?RISPARMIO DI TIPO APERTO.*?annuo- oltre)', '\b', '\s*\%');
-
+my $html = fetch_url('http://www.ikb.hr/hr/orocena-stednja');
+$mech->follow_link( text_regex => qr/letak.*kamatn.*stop.*pdf/i );
+return best_kamata_pdf_mpp('UVJETI ORO.*?ENOG NOV.*?ANOG POLOGA(.*)', '\b', '\b');
