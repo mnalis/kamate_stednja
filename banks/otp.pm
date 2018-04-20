fd6a226729a0e11cfa2dd033f8c9ef9f8ae941c3
Matija Nalis <mnalis-git@voyager.hr>
2018-04-20 10:48:17 +0200

    izbaci OTP djecju stednju samo (ostavi ostalo)

--- banks/otp.pm
+++ banks/otp.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.otpbanka.hr/hr/gradani/orocena-stednja-uz-promjenjivu-kamatnu-stopu');
 $mech->follow_link( text_regex => qr/kamatne stope/i );
-return best_kamata_pdf_mpp('(.*)Dje.*?ja planirana', '^\s*', '\s*$');
+return best_kamata_pdf_mpp('(.*)HRK Dje.*?ja planirana.*?HRK planirana(.*)', '^\s*', '\s*$');
