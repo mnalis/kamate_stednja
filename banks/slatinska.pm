f4def9483abef1559e3c8678733bb0f08baaaaa7
Matija Nalis <mnalis-git@voyager.hr>
2018-05-16 13:30:25 +0200

    slatinska banka web change

--- banks/slatinska.pm
+++ banks/slatinska.pm
@@ -1,2 +1,6 @@
-return best_kamata_html_def ('https://www.slatinska-banka.hr/stednja_gr.php');
+return best_kamata_html_mpp ('https://www.slatinska-banka.hr/stednja_gr.php',
+	undef,
+	'>\s*',
+	'\s*<'
+);
 
