531814cbb7d5f4afc5d9fcc49492702519fb6127
Matija Nalis <mnalis-git@voyager.hr>
2018-07-13 01:25:14 +0200

    find latest link!

--- banks/kentbank.pm
+++ banks/kentbank.pm
@@ -1,4 +1,20 @@
 my $html = fetch_url ('http://www.kentbank.hr/2608/orocena-stednja');
-$mech->follow_link( text_regex => qr/Letak .*tednje/i );
+
+my $last_datum = 0;
+my $last_link;
+
+# find latest link
+foreach my $link ( $mech->find_all_links( text_regex => qr/Letak .*tednje .* od /i ) ) {
+	my ($d, $m, $y) = $link->text() =~ /.*(\d{2})\.(\d{2})\.(\d{4})\./;
+	my $datum = "$y$m$d";
+	dbg 4, "test datum = $datum";
+	if ($datum > $last_datum) {
+		$last_datum = $datum;
+		$last_link = $link;
+	}
+}
+dbg 3, "last datum=$last_datum";
+
+$mech->get( $last_link->url() );
 return best_kamata_pdf_mpp('(.*)DJE.*?JA.*?OTVORENA(.*)', '\b', '\s*\%');
 
