eeb2066ac46f8f1c173e52ade788f0f30f5362d8
Matija Nalis <mnalis-git@voyager.hr>
2018-07-13 00:45:15 +0200

    fix kentbank

--- banks/kentbank.pm
+++ banks/kentbank.pm
@@ -1,2 +1,4 @@
-return best_kamata_html_def ('http://www.kentbank.hr/2608/orocena-stednja');
+my $html = fetch_url ('http://www.kentbank.hr/2608/orocena-stednja');
+$mech->follow_link( text_regex => qr/Letak .*tednje/i );
+return best_kamata_pdf_mpp('(.*)DJE.*?JA.*?OTVORENA(.*)', '\b', '\s*\%');
 
