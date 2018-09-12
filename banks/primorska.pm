d4149935b89f487f7a610beac26671038633d85b
Matija Nalis <mnalis-git@voyager.hr>
2018-09-12 14:12:46 +0200

    primorska banka u likvidaciji

--- banks/primorska.pm
+++
@@ -1,6 +0,0 @@
-return best_kamata_html_mpp ('https://www.primorska.hr/hr/gradani/stednja-gradana',
-	'Kamatne stope.*?<tbody(.*?)</tbody>.*?</table>',
-	'<td.*?>(?=<.+?>)?',
-	'\s*%\s*<.*?/td>'
-);
-
