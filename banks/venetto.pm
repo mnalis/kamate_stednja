57bd94f83d9fabafcd94565ad261bb4661c98b73
Matija Nalis <mnalis-git@voyager.hr>
2018-10-17 21:09:03 +0200

    veneto pripojen PBZ ?

--- banks/venetto.pm
+++
@@ -1,3 +0,0 @@
-my $html = fetch_url('http://www.venetobanka.hr/stednja');
-$mech->follow_link( text_regex => qr/STANDARDNA .*?TEDNJA/i );
-return best_kamata_pdf_mpp('VRSTA DEPOZITA(.*)', '\b', '\b');
