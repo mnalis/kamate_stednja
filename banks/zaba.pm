0f19971407bdd7fd59851629648e0a1612caaa86
Matija Nalis <mnalis-git@voyager.hr>
2018-11-21 23:05:56 +0100

    ZaBa now requires javascript. Do some manual hacking with hardcoded values :(

--- banks/zaba.pm
+++ banks/zaba.pm
@@ -1,4 +1,15 @@
-my $html = fetch_url('https://www.zaba.hr/home/footer/uvjeti-poslovanja/gradjani');
-$mech->follow_link( text_regex => qr/kamatne stope.*?ednju/i );
-$mech->follow_link( url_regex => qr/kamatne.*?\.pdf/i );
+my $DEBUG = $ENV{DEBUG} || 0;
+
+my $html;
+#my $html = fetch_url('https://www.zaba.hr/home/footer/uvjeti-poslovanja/gradjani');	# uses JavaScript now 20181121 :(
+my $json = fetch_url('https://www.zaba.hr/home/footer/filtriranjeUvjetaPoslovanja?k1=116&k2=130');
+$DEBUG > 8 && print "JSON=$json\n";
+if ($json =~ m{"naziv"\s*:\s*"([^"]*\.pdf)".+"idDokument"\s*:\s*(\d+).+"opis"\s*:\s*"([^"]*?kamatne stope.*?ednju[^"]*)}i) {
+	my $filename="$1"; my $id="$2"; my $desc="$3";
+	$DEBUG > 5 && print "found $1 ($3) with id=$2\n";
+	my $url = "https://www.zaba.hr/home/med/dok/${id}/${filename}";
+	$html = fetch_url($url);
+}
+#$mech->follow_link( text_regex => qr/kamatne stope.*?ednju/i );
+#$mech->follow_link( url_regex => qr/kamatne.*?\.pdf/i );
 return best_kamata_pdf_mpp(undef, '^\s*', '\s*$');
