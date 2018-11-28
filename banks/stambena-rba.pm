562a07e09ebeae963e0625438d0076d5f3a5cc55
Matija Nalis <mnalis-git@voyager.hr>
2018-11-28 22:27:06 +0100

    stambena-rba fix (i SSL HTTPS  no-verify kludge)

--- banks/stambena-rba.pm
+++ banks/stambena-rba.pm
@@ -1,6 +1,5 @@
-return best_kamata_html_mpp ('https://www.raiffeisenstambena.hr/stambena-stednja',
-	'Primjer izra(.*)se temelji na pretpostavci',
-	'EKS.*?%.*?<td>\s*',
-	'\s*</td>'
+return best_kamata_html_regex ('https://www.raiffeisenstambena.hr/stambena-stednja',
+	'Kamatna stopa(.*)Primjer izra.*?una',
+	'\b([1-9]?\d)[.,](\d{1,2})\s*%'
 );
 
