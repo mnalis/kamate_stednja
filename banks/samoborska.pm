3aabcba594ae566d12ba9eec86882e1bcf8ffc74
Matija Nalis <mnalis-git@voyager.hr>
2019-02-03 02:36:54 +0100

    DAB, samoborska nove stranice

--- banks/samoborska.pm
+++ banks/samoborska.pm
@@ -1,3 +1,3 @@
-my $html = fetch_url('https://www.sabank.hr/kunska-tednja/c1s62/');
+my $html = fetch_url('http://www.sabank.hr/kunska-tednja/c62s62/');
 $mech->follow_link( url_regex => qr/stednja.*?\.pdf/i );
 return best_kamata_pdf_def();
