45b3d46994ca15244382b43099dac2fd029ec419
Matija Nalis <mnalis-git@voyager.hr>
2019-06-05 14:32:30 +0200

    BKS URL changed

--- banks/bks.pm
+++ banks/bks.pm
@@ -1,3 +1,3 @@
-my $html = fetch_url('http://bks.hr/BKSWebp/BKS/bks_hr/Gradanstvo/Stednja/Orocena_stednja/index.jsp');
+my $html = fetch_url('https://www.bks.hr/orocena-stednja');
 $mech->follow_link( url_regex => qr/kamatne_stope.*?\.pdf/i );
 return best_kamata_pdf_def();
