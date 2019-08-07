cd049398dfcccacc1e50b61eff4682f686d9722e
Matija Nalis <mnalis-git@voyager.hr>
2019-08-07 10:57:19 +0200

    PBZ izmjena web stranica

--- banks/pbz.pm
+++ banks/pbz.pm
@@ -1,3 +1,3 @@
-my $html = fetch_url('https://www.pbz.hr/hr/gradani/stednja');
-$mech->follow_link( text_regex => qr/kamatnim stopama/i );
+my $html = fetch_url('https://www.pbz.hr/gradjani/orocena-stednja.html');
+$mech->follow_link( text_regex => qr/kamatne stope/i );
 return best_kamata_pdf_mpp('(.*?)e-Perspektiva.*?maksimalni iznos(.*?)Premium invest.*?inimalni iznos(.*)', '^', '\s*$');
