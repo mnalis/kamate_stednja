fa57fc01fdfa39825d83e1c04a7bc4676a024cbc
Matija Nalis <mnalis-git@voyager.hr>
2019-09-12 15:03:03 +0200

    fix Podravska banka, dozvoli strane valute (kuna 0.60% fiksno, dolar 1.30% fiksno!)

--- banks/podravska.pm
+++ banks/podravska.pm
@@ -1,5 +1,5 @@
 return best_kamata_html_mpp ('http://www.poba.hr/index.php?cat=godisnje_kamatne_stope_gradana',
-	'Kunski oro.*? depoziti.*?<table(.*?)</table>',
+	'Kunski i devizni oro.*? depoziti.*?<table(.*?)Efektivne kamatne stope jednake',
 	'<td.*?>',
 	'<.*?/td>'
 );
