bc9f4336d54685848524aef9f6b6ec43a3c246f7
Matija Nalis <mnalis-git@voyager.hr>
2020-02-27 13:23:59 +0100

    support for modifying user agent as requested

--- banks/stambena-rba.pm
+++ banks/stambena-rba.pm
@@ -1,5 +1,9 @@
+{
+	#local $USER_AGENT = $USER_AGENT_FAKE;
+	local $USER_AGENT = '';
+	dbg 3, "local UA=$USER_AGENT";
 return best_kamata_html_regex ('https://www.raiffeisenstambena.hr/stambena-stednja',
 	'Kamatna stopa(.*)Primjer izra.*?una',
 	'\b([1-9]?\d)[.,](\d{1,2})\s*%'
 );
-
+}
