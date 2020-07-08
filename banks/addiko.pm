b5c6cd6845d42314e7b3fc704f818ee781a6b934
Matija Nalis <mnalis-git@voyager.hr>
2020-07-09 01:36:08 +0200

    Addiko nove stranice (ukinuta stednja s premijom)

--- banks/addiko.pm
+++ banks/addiko.pm
@@ -1,5 +1,3 @@
-return best_kamata_html_mpp ('https://www.addiko.hr/gradanstvo/stednja-i-ulaganje/stednja-s-premijom/',
-	undef,
-	'<td>\s*',
-	'\s*</td>\s*</tr>'
-);
+my $html = fetch_url('https://www.addiko.hr/gradanstvo/stednja-i-ulaganje/orocena-stednja/');
+$mech->follow_link( url_regex => qr/kamatne-stope.*?\.pdf/i );
+return best_kamata_pdf_mpp ('kamatne stope na oročene depozite(.*)PREMIJOM ugovorene do', '\b', '\s*\%');
