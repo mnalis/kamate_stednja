ea443a182c0d13df987e6aa1675ae5fbed733053
Matija Nalis <mnalis-git@voyager.hr>
2021-05-05 18:07:08 +0200

    HPB update web

--- banks/hpb.pm
+++ banks/hpb.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.hpb.hr/opci-uvjeti-poslovanja');
-$mech->follow_link( url_regex => qr/kamatne_stope.*?\.pdf/i );
+$mech->follow_link( url_regex => qr/VISINI%20KS.*\.pdf/i );
 return best_kamata_pdf_mpp('NENAMJENSKI.*?DEPOZIT(.*?)DEPOZIT S .*?EKRATNIM UPLATAMA', '\b', '\s*\%\s*[^*]');
