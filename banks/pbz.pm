507da07323399c20765fde99188881041f8cdbbb
Matija Nalis <mnalis-git@voyager.hr>
2021-06-02 13:49:11 +0200

    debug PBZ add

--- banks/pbz.pm
+++ banks/pbz.pm
@@ -1,3 +1,8 @@
+my $DEBUG = $ENV{DEBUG} || 9;
+
+$DEBUG > 6 && print "about to fetch html\n";
 my $html = fetch_url('https://www.pbz.hr/gradjani/orocena-stednja.html');
+$DEBUG > 6 && print "searching html\n";
 $mech->follow_link( text_regex => qr/kamatne stope/i );
+$DEBUG > 6 && print "searching pdf\n";
 return best_kamata_pdf_mpp(undef, '^', '\s*$');
