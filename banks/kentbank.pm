e3481e3f1c606a5f6b37bd0698cfc2a9587f18b5
Matija Nalis <mnalis-git@voyager.hr>
2021-11-24 15:22:13 +0100

    update kentbank

--- banks/kentbank.pm
+++ banks/kentbank.pm
@@ -1,16 +1,21 @@
 my $html = fetch_url ('http://www.kentbank.hr/2608/orocena-stednja');
 
 my $last_datum = 0;
-my $last_link;
+my $last_link = undef;
 
 # find latest link
-foreach my $link ( $mech->find_all_links( text_regex => qr/Letak .*tednj.* od /i ) ) {
-	my ($d, $m, $y) = $link->text() =~ /.*(\d{2})\.(\d{2})\.(\d{4})\./;
-	my $datum = "$y$m$d";
-	dbg 4, "test datum = $datum";
-	if ($datum > $last_datum) {
-		$last_datum = $datum;
+foreach my $link ( $mech->find_all_links( text_regex => qr/Letak .*tednj/i ) ) {
+	if (!defined $last_link) {
 		$last_link = $link;
+	}
+
+	if (my ($d, $m, $y) = $link->text() =~ /.*(\d{2})\.(\d{2})\.(\d{4})\./) {
+		my $datum = "$y$m$d";
+		dbg 4, "test datum = $datum";
+		if ($datum > $last_datum) {
+			$last_datum = $datum;
+			$last_link = $link;
+		}
 	}
 }
 dbg 3, "last datum=$last_datum";
