71dbc89251f2e21e6c646a939031c3427bcb474d
Matija Nalis <mnalis-git@voyager.hr>
2022-04-20 16:28:07 +0200

    Sberbank -> Nova Hrvatska Banka; preuzela ih HPB grupacija

---
+++ banks/novahrvatskabanka.pm
@@ -0,0 +1,1 @@
+return best_kamata_html_mpp ('https://www.novahrvatskabanka.hr/gradjani/stednja-i-ulaganje/happy-stednja/', undef, 'EKS iznosi\s+', '\s*\%');
--- banks/sberbank.pm
+++
@@ -1,2 +0,0 @@
-return best_kamata_html_def ('https://www.sberbank.hr/gradjani/stednja-i-ulaganje/standard-stednja/');
-
