bb0717f27290b1bbf13f889a3bdea1803c457566
Matija Nalis <mnalis-git@voyager.hr>
2023-01-11 13:59:05 +0100

    addiko fix

--- banks/addiko.pm
+++ banks/addiko.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.addiko.hr/gradanstvo/stednja-i-ulaganje/orocena-stednja/');
 $mech->follow_link( url_regex => qr/kamatne-stope.*?\.pdf/i );
-return best_kamata_pdf_mpp ('kamatne stope na oročene depozite(.*)PREMIJOM ugovorene do', '\b', '\s*\%');
+return best_kamata_pdf_mpp ('(.*)Novi depoziti u valuti USD ne ugovaraju se za razred', '\b', '\s*\%');
