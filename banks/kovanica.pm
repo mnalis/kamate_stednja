3080f0420f7368041681b8e93f0c3185377e3d29
Matija Nalis <mnalis-git@voyager.hr>
2023-01-12 03:56:58 +0100

    Kovanica: akcijska stednja novo!

--- banks/kovanica.pm
+++ banks/kovanica.pm
@@ -1,2 +1,4 @@
-return best_kamata_html_def ('https://www.kovanica.hr/gradanstvo/stednja/orocena-stednja');
+my $k1 = best_kamata_html_def ('https://www.kovanica.hr/gradanstvo/stednja/orocena-stednja');
+my $k2 = best_kamata_html_def ('https://www.kovanica.hr/gradanstvo/stednja/orocena-stednja/akcijska-stednja');
 
+return ($k1 > $k2) ? $k1 : $k2;
