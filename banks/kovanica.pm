f120174cf4c0303c49d0ef4c44e63659320247af
Matija Nalis <mnalis-git@voyager.hr>
2023-04-05 18:24:49 +0200

    Kovanica - maknuta akcijska stednja sa stranica

--- banks/kovanica.pm
+++ banks/kovanica.pm
@@ -1,4 +1,4 @@
 my $k1 = best_kamata_html_def ('https://www.kovanica.hr/gradanstvo/stednja/orocena-stednja');
-my $k2 = best_kamata_html_def ('https://www.kovanica.hr/gradanstvo/stednja/orocena-stednja/akcijska-stednja');
+my $k2 = best_kamata_html_def ('https://www.kovanica.hr/gradanstvo/stednja/stednja-po-videnju/a-vista-plus');
 
 return ($k1 > $k2) ? $k1 : $k2;
