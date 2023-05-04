001ecf68d87c2b65c6209c451ab16c9e069e7885
Matija Nalis <mnalis-git@voyager.hr>
2023-05-04 03:52:16 +0200

    fix Erste regex

--- banks/erste.pm
+++ banks/erste.pm
@@ -1,5 +1,5 @@
 return best_kamata_html_mpp ('https://www.erstebank.hr/hr/gradjanstvo/stednja-i-investicije/stednja/standardna-stednja#uvjeti',
-	'kamatna stopa(.*)</div>',
+	'Kamatna stopa(.*)</div>',
 	'\b',
 	'\s*\%'
 );
