e54cb9ce2f214e3a8117628170ae6aa17df80a51
Matija Nalis <mnalis-git@voyager.hr>
2023-06-14 18:29:19 +0200

    stambena-wustenrot ignore HTML formatting

--- banks/stambena-wustenrot.pm
+++ banks/stambena-wustenrot.pm
@@ -1,5 +1,4 @@
 return best_kamata_html_regex ('http://wuestenrot.hr/stambena-stednja/tarifa-spore-stednje/33',
 	undef,
-	'\b([1-9]?\d)[.,](\d{1,2})\s*%'
+	'\b([1-9]?\d)[.,](\d{1,2})\s*(?:<[^>]+>)?(?:&[^;]+;)?(?:</[^>]+>)?\s*%'
 );
-
