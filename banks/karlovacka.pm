becb499e471d3729b93cda5e8a2be3536996f3a3
Matija Nalis <mnalis-git@voyager.hr>
2023-09-15 00:42:10 +0200

    fix kaba.hr URL

--- banks/karlovacka.pm
+++ banks/karlovacka.pm
@@ -1,4 +1,4 @@
-my $html = fetch_url('http://www.kaba.hr/gradanstvo/orocena-kunska-stednja/');
+my $html = fetch_url('https://www.kaba.hr/gradanstvo/orocena-eur-stednja/');
 $mech->follow_link( url_regex => qr/bonus-.*?tednja.*?\.pdf/i );
 return best_kamata_pdf_mpp ('depozit(.*)raskid', '\b', '\s*\%');
 
