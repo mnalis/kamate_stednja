cd6539bd77e41db484233977b5bd75f28e018671
Matija Nalis <mnalis-git@voyager.hr>
2023-10-13 01:28:03 +0200

    IKB update

--- banks/ikb.pm
+++ banks/ikb.pm
@@ -1,3 +1,3 @@
-my $html = fetch_url('http://www.ikb.hr/hr/orocena-stednja');
-$mech->follow_link( text_regex => qr/letak.*?kamatn.*?stop.*PDF/i );
-return best_kamata_pdf_mpp('UVJETI ORO.*?ENOG NOV.*?ANOG POLOGA(.*)', '\b', '\b');
+my $html = fetch_url('https://ikb.hr/gradani/korisne-informacije/opci-uvjeti/kamatne-stope');
+$mech->follow_link( text_regex => qr/kamatne stope na depozite/i );
+return best_kamata_pdf_mpp('(.*)OTVORENA DJE', '\b', '\s*\%');
