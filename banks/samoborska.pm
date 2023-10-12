9b0853403fd0031e9c31dcc1445e77ca7ef8cef4
Matija Nalis <mnalis-git@voyager.hr>
2023-10-13 01:21:32 +0200

    samoborska novi PDF

--- banks/samoborska.pm
+++ banks/samoborska.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.sabank.hr/devizna---tednja/c61s4/');
-$mech->follow_link( url_regex => qr/stednja.*?\.pdf/i );
+$mech->follow_link( url_regex => qr/oroc_depoz.*?\.pdf/i );
 return best_kamata_pdf_def();
