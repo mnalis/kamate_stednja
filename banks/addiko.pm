624efd3c1e7fa68c9a29c052710f180866421bd9
Matija Nalis <mnalis-git@voyager.hr>
2024-02-16 03:02:13 +0100

    Addiko fix

--- banks/addiko.pm
+++ banks/addiko.pm
@@ -1,3 +1,4 @@
 my $html = fetch_url('https://www.addiko.hr/gradanstvo/stednja-i-ulaganje/orocena-stednja/');
+$mech->follow_link( url_regex => qr/naknade-gradanstvo/i );
 $mech->follow_link( url_regex => qr/kamatne-stope.*?\.pdf/i );
 return best_kamata_pdf_mpp ('kamatne stope na .*? oro(.*)', '\b', '\s*\%');
