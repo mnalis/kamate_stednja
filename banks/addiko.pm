6ef980b6d1bddb2617fcbcc1e4fd937293d9a477
Matija Nalis <mnalis-git@voyager.hr>
2024-07-29 14:17:07 +0200

    Addiko - ako ima vise PDFova, slijedi zadnji (bila bi bolja pametnija logika koja po datumu zakljucuje, ali ok...)

--- banks/addiko.pm
+++ banks/addiko.pm
@@ -1,4 +1,9 @@
 my $html = fetch_url('https://www.addiko.hr/gradanstvo/stednja-i-ulaganje/orocena-stednja/');
+my $pdf_regex = qr/kamatne-stope.*?\.pdf/i;
+
 $mech->follow_link( url_regex => qr/naknade-gradanstvo/i );
-$mech->follow_link( url_regex => qr/kamatne-stope.*?\.pdf/i );
+@pdfs = $mech->find_all_links( url_regex => $pdf_regex );
+my $last_pdf = scalar @pdfs;
+
+$mech->follow_link( url_regex => $pdf_regex, n => $last_pdf );
 return best_kamata_pdf_mpp ('kamatne stope na .*? oro(.*)', '\b', '\s*\%');
