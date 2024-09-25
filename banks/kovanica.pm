51b8aec7327495bfc288053add618f3e9343d6de
Matija Nalis <mnalis-git@voyager.hr>
2024-09-25 15:50:41 +0200

    fix integer kamata (like "2%" instead of "2.00%" in Kovanica freedom štednja)

--- banks/kovanica.pm
+++ banks/kovanica.pm
@@ -1,4 +1,3 @@
 my $html = fetch_url('https://www.kovanica.hr/gradanstvo/stednja/orocena-stednja');
 
-# FIXME - ne pronalazi kamate bez decimalnog mjesta, npr. 2% u FREEDOM stednji
-return max_of_all_pdf_links (undef, '\b([1-9]?\d)[.,](\d{2})\s*\%', undef, $mech->find_all_links(text_regex => qr/(doplatna|profitna|rentna|freedom).*ednja.*\.pdf/));
+return max_of_all_pdf_links (undef, '\b([1-9]?\d)(?:[.,](\d{2}))?\s*\%', undef, $mech->find_all_links(text_regex => qr/(doplatna|profitna|rentna|freedom).*ednja.*\.pdf/));
