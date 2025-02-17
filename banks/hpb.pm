0c9945d0aafe840240311a969ef57f759a368322
Matija Nalis <mnalis-git@voyager.hr>
2025-02-17 04:21:40 +0100

    fix parsing da ne gleda istekle stednje (needs manual tweaks)

--- banks/hpb.pm
+++ banks/hpb.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.hpb.hr/hr/stednja/552');
 $mech->follow_link( url_regex => qr/VISINI(%20| )KS.*\.pdf/i );
-return best_kamata_pdf_mpp('ORO.*?DEPOZIT(.*?)depozite otvorene do 12. lipnja 2017. godine', '\b', '\s*\%\s*[^*]');
+return best_kamata_pdf_mpp('ORO.*?DEPOZIT(.*?)se ne ugovara od.*?EDNJA 4(.*?)depozite otvorene do 12. lipnja 2017. godine', '\b', '\s*\%\s*[^*]');
