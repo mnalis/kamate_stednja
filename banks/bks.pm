d01232363434f0c29fca1cc6370583cd78d89566
Matija Nalis <mnalis-git@voyager.hr>
2025-02-19 18:34:46 +0100

    BKS:: fix PDF, onaj sa aktivnim stopama je za kredite samo

--- banks/bks.pm
+++ banks/bks.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.bks.hr/footer/naknade-i-kamatne-stope');
-$mech->follow_link( text_regex => qr/aktivnim kamatnim stopama za stanov/i );
+$mech->follow_link( text_regex => qr/pasivnim kamatnim stopama za stanov/i );
 return best_kamata_pdf_def();
