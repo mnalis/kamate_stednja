c5e8240f1676f7a210b94b13ab2146f63183a7ac
Matija Nalis <mnalis-git@voyager.hr>
2025-05-21 23:51:49 +0200

    RBA update URL

--- banks/rba.pm
+++ banks/rba.pm
@@ -1,3 +1,3 @@
-my $html = fetch_url('https://www.rba.hr/stednja-i-ulaganje/orocena-stednja/dokumentacija');
+my $html = fetch_url('https://www.rba.hr/hr/gradani/proizvodi-i-usluge/stednja-i-ulaganje/orocena-stednja.html');
 $mech->follow_link( text_regex => qr/kamatne stope na oro.*?ednju gra/i );
 return best_kamata_pdf_mpp('VRSTA DEPOZITA(.*)', '\b', '\s');
