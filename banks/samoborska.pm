49f0879c46eef2a84b2f44746254f20d1c479e16
Matija Nalis <mnalis-git@voyager.hr>
2025-07-02 22:27:58 +0200

    Samoborska web update

--- banks/samoborska.pm
+++ banks/samoborska.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.sabank.hr/devizna---tednja/c61s4/');
-$mech->follow_link( url_regex => qr/oroc_depoz.*?\.pdf/i );
+$mech->follow_link( url_regex => qr/oro.*depoz.*?\.pdf/i );
 return best_kamata_pdf_def();
