d5deb01cd917c8fcf39dd7a2514b5f86dfa34ffb
Matija Nalis <mnalis-git@voyager.hr>
2025-10-08 11:47:53 +0200

    change IKB

--- banks/ikb.pm
+++ banks/ikb.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://ikb.hr/gradani/korisne-informacije/opci-uvjeti/kamatne-stope');
-$mech->follow_link( text_regex => qr/kamatne stope na depozite/i );
+$mech->follow_link( text_regex => qr/kamatne stope na .*pologe/i );
 return best_kamata_pdf_mpp('(.*)OTVORENA DJE', '\b', '\s*\%');
