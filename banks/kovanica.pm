6038f0bfd87de9d4ec37bf095173ff2dded66575
Matija Nalis <mnalis-git@voyager.hr>
2026-03-18 16:29:09 +0100

    banka Kovanica update

--- banks/kovanica.pm
+++ banks/kovanica.pm
@@ -1,3 +1,3 @@
 my $html = fetch_url('https://www.kovanica.hr/gradanstvo/stednja/orocena-stednja');
 
-return max_of_all_pdf_links (undef, '\b([1-9]?\d)(?:[.,](\d{2}))?\s*\%', undef, $mech->find_all_links(text_regex => qr/(doplatna|profitna|rentna|freedom).*ednja.*\.pdf/));
+return max_of_all_pdf_links (undef, '\b([1-9]?\d)(?:[.,](\d{2}))?\s*\%', undef, $mech->find_all_links(text_regex => qr/(doplatna|profitna|rentna|freedom).*ednja.*\.pdf/i));
