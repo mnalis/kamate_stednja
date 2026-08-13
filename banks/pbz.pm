898d4cfdeda98ea1543bf4cc80165d50cdeb327a
Matija Nalis <mnalis-git@voyager.hr>
2026-08-13 10:37:48 +0200

    fake PBZ user-agent, as default Wget one is blocked

--- banks/pbz.pm
+++ banks/pbz.pm
@@ -1,5 +1,5 @@
 dbg 6, 'about to fetch html';
-my $html = fetch_url('https://www.pbz.hr/gradjani/orocena-stednja.html');
+my $html = fetch_url('https://www.pbz.hr/gradjani/orocena-stednja.html',1);
 dbg 6, 'searching html';
 $mech->follow_link( text_regex => qr/kamatne stope/i );
 dbg 6, 'searching pdf';
