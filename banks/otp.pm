735126d9dec2a212b09641c420fb71768feaf2d3
Matija Nalis <mnalis-git@voyager.hr>
2026-08-13 10:36:25 +0200

    OTP use fake user-agent, as Wget one is blocked

--- banks/otp.pm
+++ banks/otp.pm
@@ -1,3 +1,3 @@
-my $html = fetch_url('https://www.otpbanka.hr/hr/gradani/planirana-stednja');
+my $html = fetch_url('https://www.otpbanka.hr/gradani/planirana-stednja', 1);
 $mech->follow_link( text_regex => qr/kamatnom stopom/i );
 return best_kamata_pdf_mpp('(.*)', '^\s*', '\s*$');
